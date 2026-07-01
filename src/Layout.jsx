055f6aa3b415fa63d80de068ca4f4987ab207390
AnmolMehta435 <anmolmehtam1234@gmail.com>
2026-07-01 20:15:46 +0530

    Fix header layput and srcoll to top on navigation

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import Header from "./components/Header/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
+import ScrollToTop from "./ScrollToTop";
+
 function Layout() {
     return(
         <>
+        <ScrollToTop />
         <Header/>
         <Outlet/>
         <Footer/>
